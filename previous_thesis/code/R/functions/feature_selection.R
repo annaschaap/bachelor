d3888302991ef401b8d7850f6660de1a51ac774d
annaschaap <112488625+annaschaap@users.noreply.github.com>
2024-09-11 12:59:01 +0200

    previous thesis code

---
+++ previous_thesis/code/R/functions/feature_selection.R
@@ -0,0 +1,65 @@
+#Lasso loop
+elastic <- function(train_data, 
+                           folds, 
+                           id_col, 
+                           featureset = featureset, 
+                           language,
+                           hold_set,
+                           task,
+                           demo_set){
+  #parition using groupdata 2, keeping IDs in same fold
+  fold_train <- train_data %>% 
+    groupdata2::fold(.,
+                     k = folds,
+                     id_col = id_col)  #new col called .fold
+
+  feature_list = NULL #Make empty list for storing selected features
+  for(i in (1:length(unique(fold_train$.folds)))){
+    print(paste("now looping through fold", i, sep = " "))
+    lasso_train <- fold_train  %>% 
+      filter(.folds != i) %>% 
+      select(-c(id_col))
+    
+    lasso_test <- fold_train  %>% 
+      filter(.folds == i)
+    
+    ###DEFINING VARIABLES
+    lasso_data <- lasso_train %>% select(-c(story_type, condition, Gender, 
+                                            .folds, country, feature_set))
+    x <- model.matrix(Diagnosis ~ ., data = lasso_data) #making a matrix from formula
+    y <- lasso_train$Diagnosis #choosing the dependent variable
+    
+    ###FEATURE SELECTION
+    set.seed(1234) #set seed
+    cv_lasso <- cv.glmnet(x, 
+                          y, 
+                          alpha = 0.5, # Setting alpha between 0 and 1 implements elastic
+                          standardize = F,
+                          family = "binomial",
+                          type.measure = "auc")
+    
+    ###EXTRACTING COEFFICIENTS
+    lasso_coef <- tidy(cv_lasso$glmnet.fit) %>%  
+      filter(lambda == cv_lasso$lambda.1se,
+             term != "(Intercept)") %>% #we do not want the intercept
+      select(term, estimate) %>% 
+      mutate(abs = abs(estimate),
+             term = str_remove_all(term, "`"), #clean the term string
+             lambda_1se = paste(cv_lasso$lambda.1se),
+             test_fold = paste(i)) %>% 
+      filter(abs > 0)
+    
+    # return(cv_lasso) # Do this if you want to get the lambda plot
+    
+    # #selecting columns to keep in csv file
+    lists <- data.frame(features = ifelse(str_detect(lasso_coef$term, ".folds") == F,
+                                          lasso_coef$term,
+                                          NA))
+    lists <- subset(lists, !is.na(lists$features))
+    lists$fold <- paste(i)
+    feature_list <- rbind(feature_list, lists)
+  }
+  #writing the csvs
+  write.csv(fold_train, paste(paste("../data/speech_data/data", featureset, language, task, sep = "_"), "csv", sep = "."))
+  write.csv(feature_list, paste(paste("../data/feature_lists/features", featureset, language, task, sep = "_"),"csv", sep = "."))
+}
---
+++ previous_thesis/code/R/functions/id_wrangl.R
@@ -0,0 +1,43 @@
+id_wrangling <- function(feature, dem, other, language){
+  if (language == "dk"){
+      #extracting IDs
+      feature <- feature %>% 
+        mutate(ID = as.character(str_extract(ID, "[0-9]*")))
+    
+      ###Clean other data
+      other <- other %>% 
+        mutate(
+          ID = as.character(ID),  #Making ID character
+          ID_letter = str_extract(ID, "[A-Z]+"), #extracting letters from ID name
+          ID_number = str_extract(ID, "[0-9]*")  #extracting numbers from ID name
+        )
+      other$ID_letter <- ifelse(is.na(other$ID_letter), "", 
+                                          other$ID_letter)
+      other <- other %>% 
+        unite(ID, ID_letter:ID_number, sep = "") 
+  }
+  
+  if (language == "us"){
+    feature <- feature %>% 
+      mutate(
+        ID = as.character(ID),  #Making ID character
+        ID_letter = str_extract(ID, "[A-Z]+"), #extracting letters from ID name
+        ID_number = str_extract(ID, "[0-9]*"))  #extracting numbers from ID name
+    
+    feature$ID_letter <- ifelse(is.na(feature$ID_letter), "", feature$ID_letter)
+    
+    feature <- feature %>% 
+      unite(ID, ID_letter:ID_number, sep = "") 
+    
+    #Clean other
+    other <- other %>% 
+      mutate(ID = as.character(str_extract(ID, "[0-9]*")))
+  }
+  dem <- dem %>% subset(ID != "103"& ID != "104"& ID !="105"& ID !="111"& ID !="114")
+  #Removing columns that we will not be needing
+  feature <- feature %>% select(-c(name, trial, frameTime))
+  other <- other %>% select(-c(name, trial, frameTime))
+  #Ensuring that ID is a character
+  other$ID <- as.character(other$ID)
+  return(list(feature, other, dem))
+}
